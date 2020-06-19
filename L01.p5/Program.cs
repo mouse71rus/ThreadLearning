5f94a8cfb5a5db5f5249a8b2909c7ef5a210c4f5
mouse71rus <zhimerin98007@gmail.com>
2020-06-19 16:02:53 +0300

    Lesson: Background Thread

---
+++ L01.p5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L01.p5
+{
+    class Program
+    {
+        static void Log()
+        {
+            while (true)
+            {
+                Console.WriteLine($"Log: {DateTime.Now.ToLongTimeString()}");
+                Thread.Sleep(100);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("L01.p5");
+            Console.WriteLine();
+
+            Thread t = new Thread(Log);
+            t.Start();
+            t.IsBackground = true; // Specify that this thread is background
+
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("\tMain thread");
+                Thread.Sleep(100);
+            }
+
+            
+            // Application will stop the thread "t" here
+        }
+    }
+}
