11a876966a132ffb0075a02ab58b5f32ed576642
mouse71rus <zhimerin98007@gmail.com>
2020-06-19 13:18:08 +0300

    First commit

---
+++ L01.p1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L01.p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("L01.p1");
+            Console.WriteLine();
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
