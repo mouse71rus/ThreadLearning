c729d08837646885e9d7d0445ee7f841701ca202
mouse71rus <zhimerin98007@gmail.com>
2020-06-22 12:02:20 +0300

    ThreadStatic attribute

---
+++ L02.p3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L02.p3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("L02.p3");
+            Console.WriteLine();
+
+            Method();
+
+            Console.ReadKey();
+        }
+
+        private static void Method()
+        {
+            if(Provider.Data < 10)
+            {
+                Provider.Data++;
+
+                var th = Thread.CurrentThread;
+                Console.WriteLine($"\nThread ID: {th.ManagedThreadId} was started");
+
+                var thirdThread = new Thread(Method);
+                thirdThread.Start();
+                thirdThread.Join();
+
+                Console.WriteLine($"\nThread ID: {th.ManagedThreadId} was finished");
+            }                     
+        }
+    }
+}
---
+++ L02.p3/Provider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace L02.p3
+{
+    internal class Provider
+    {
+        [ThreadStatic] // Using this attribute give you unique field for every Thread
+        internal static int Data;
+    }
+}
