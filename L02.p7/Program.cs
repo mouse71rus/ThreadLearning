0656f6f49197d0738e28b0d8eafb077f9413d103
mouse71rus <zhimerin98007@gmail.com>
2020-06-22 14:16:53 +0300

    Thread.Priority

---
+++ L02.p7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L02.p7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("L02.p7");
+            Console.WriteLine();
+            
+            ThreadWrapper[] threads = new ThreadWrapper[]
+            {
+                new ThreadWrapper() {Priority = ThreadPriority.Lowest},
+                new ThreadWrapper() {Priority = ThreadPriority.Highest},
+                new ThreadWrapper() {Priority = ThreadPriority.Normal},
+            };
+
+            foreach (var item in threads)
+            {
+                item.Start();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L02.p7/ThreadWrapper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace L02.p7
+{
+    class ThreadWrapper
+    {
+        private static bool endCalculations;
+        private Thread t;
+        private int args;
+
+        public ThreadPriority Priority { set { t.Priority = value; } }
+
+        static ThreadWrapper()
+        {
+            endCalculations = false;
+        }
+
+        public ThreadWrapper()
+        {
+            t = new Thread(Calc);
+            args = 1_000_000; // LOL. It's work
+        }
+
+        private void Calc()
+        {
+            while (!endCalculations && args != 0)
+            {
+                args--;
+            }
+
+            endCalculations = true;
+            Console.WriteLine($"\nThread ID: {Thread.CurrentThread.ManagedThreadId} was finished. Priority: {t.Priority}. Args: {args}");
+        }
+
+        public void Start()
+        {
+            t.Start();
+        }
+    }
+}
