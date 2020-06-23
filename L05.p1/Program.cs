b22b8ba81faab821ec4a15024b3d6ddaf64a5ba7
mouse71rus <zhimerin98007@gmail.com>
2020-06-23 13:44:41 +0300

    TPL

---
+++ L05.p1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L05.p1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("L05.p1");
+            Console.WriteLine();
+
+            var a = new Action(Method);
+            var task = new Task(a);
+            task.Start();
+            for (int i = 0; i < 100; i++)
+            {
+                Console.Write("- ");
+                Thread.Sleep(50);
+            }
+
+            Console.WriteLine("Main() was finished");
+
+            Console.ReadKey();
+        }
+
+        private static void Method()
+        {
+            Console.WriteLine("Method working..");
+
+            for (int i = 0; i < 20; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine($"Method: i = {i}");
+            }
+
+            Console.WriteLine("Method() was finished");
+        }
+    }
+}
---
+++ L05.p2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace L05.p2
+{
+    class Program
+    {
+        private static void Method()
+        {
+            Console.WriteLine($"Method() CurrentID: {Task.CurrentId} working..");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(100);
+                Console.WriteLine($"Method #{Task.CurrentId}: i = {i}");
+            }
+
+            Console.WriteLine($"Method() CurrentID: {Task.CurrentId} was finished");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            Console.WriteLine("L05.p2");
+            Console.WriteLine();
+            
+            var task1 = new Task(Method);
+            var task2 = new Task(Method);
+
+            task1.Start();
+            task2.Start();
+
+            Console.WriteLine($"task1: {task1.Id}");
+            Console.WriteLine($"task2: {task2.Id}");
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("- ");
+                Thread.Sleep(50);
+            }
+
+            Console.WriteLine("Main() was finished");
+
+            // Главный поток не будет дожидаться окончания task1, task2
+            //Console.ReadKey();
+        }
+    }
+}
